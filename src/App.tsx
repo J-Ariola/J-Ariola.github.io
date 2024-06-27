4eee7632278fb23e5971ba0437a2780bc18402c3
Jarrod Ariola <jarrod.ariola@gmail.com>
2024-06-27 14:30:15 +0900

    feat: update about me and add japanese translation

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,11 @@ function App() {
         <h1 className="text-5xl font-sans">{t("header")}</h1>
       </header>
       <Navigation/>
-      <h2>{t("introduction.part1")}</h2>
+      <h2>{t("introduction.part_1")}</h2>
       <section>
         <h2 className="text-3xl font-sans">{t("navigation_menu.about_me")}</h2>
-        <p>{t("about_me.main")}</p>
+        <p>{t("about_me.part_1")}</p>
+        <p>{t("about_me.part_2")}</p>
         <p>{t("about_me.closing")} <a>{t("contact_info.email")}</a></p>
       </section>
       <section>
