aa7aa2de4d4460246e2c459c319b63404c7b0f83
Jarrod Ariola <jarrod.ariola@gmail.com>
2024-07-02 14:57:23 +0900

    feat: add grid to project badges

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,14 @@ import { useTranslation } from "react-i18next";
 import Navigation from './components/react/Navigation';
 import ProjectCarousel from "./components/react/ProjectCarousel";
 import { Project } from "@/@types/global";
-import NextJSLogo from "../public/img/badge/node-js.logo.svg";
+import NextJSLogo from "../public/img/badges/next-js.logo.svg";
+import NodeJSLogo from "../public/img/badges/node-js.logo.svg";
+import PrismaLogo from "../public/img/badges/prisma.logo.svg";
+import FirebaseLogo from "../public/img/badges/firebase.logo.svg";
+import JestLogo from "../public/img/badges/jest.logo.svg";
+import ProtoMapsLogo from "../public/img/badges/protomaps.logo.svg";
+import { Button } from "./components/ui/button";
+import ExternalLinkIcon from "../public/img/icons/external-link.icon.svg";
 
 const projectNameImgFileNameLookUp = {
   "PoiPoi": "poipoi.png",
@@ -50,13 +57,24 @@ function App() {
           <ProjectCarousel projects={projects}/>
         </section>
         {/* TODO: Each section of the project will be inserted here */}
-        <section>
-          <div className="flex bg-purple-950/50 rounded-lg ">
+        <section className="">
+          <div className="flexbg-purple-950/50 rounded-lg w-full justify-center">
             <div className="">
-              <h2 className="text-3xl">{"Project Name"}</h2>
-              <p>{"Description of the App"}</p>
-              <div className="grid-rows-2">
+              <div className="flex justify-center">
+              <h2 className="text-3xl pr-6">{"Project Name"}</h2>
+              <Button asChild variant="outline" size="icon" onClick={() => alert("External Link Clicked")}>
+                <img className="w-8" src={ExternalLinkIcon} alt="Link to Project" />
+              </Button>
+              </div>
+              <p className="text-2xl">{"This will be a small description of the app."}</p>
+              <div className="grid grid-cols-4 gap-2 w-56">
+                <img className="bg-white rounded-lg" src={NodeJSLogo} alt="Node JS Logo" ></img>
+                <img className="bg-white rounded-lg" src={PrismaLogo} alt="Prisma Logo" ></img>
                 <img src={NextJSLogo} alt="Next JS Logo" ></img>
+                <img src={FirebaseLogo} alt="Firebase Logo" ></img>
+                <img src={JestLogo} alt="Jest Logo" ></img>
+                <img src={ProtoMapsLogo} alt="Protomaps Logo" ></img>
+
               </div>
             </div>
           </div>
