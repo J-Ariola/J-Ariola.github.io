4e5fd4cd625f6f82aa528ac381e920eaee27f1c2
Jarrod Ariola <jarrod.ariola@gmail.com>
2024-07-03 18:43:23 +0900

    feat: loop carousel and change carousel image size

--- src/components/react/ProjectCarousel.tsx
+++ src/components/react/ProjectCarousel.tsx
@@ -7,22 +7,31 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel"
 import { useTranslation } from "react-i18next"
+import { start } from "repl";
 
 interface Props {
-  projects: Project[]
+  projects: Project[],
+  className: string,
 }
 
-export default function ProjectCarousel({projects}: Props) {
+export default function ProjectCarousel({projects, className}: Props) {
   const { t } = useTranslation();
 
   return (
-    <Carousel className="w-full max-w-xs">
+    <Carousel 
+    className={className}
+    opts={{
+      align: "start",
+      loop:true,
+    }}>
       <CarouselContent>
         {projects.map((project, index) => (
-          <CarouselItem key={index}>
+          <CarouselItem 
+          key={index}
+          >
             <a href="/">
               <div> {t(`project_titles.${project.titleKey}`)} </div>
-              {project.imgUrl ? <img src={`/img/${project.imgUrl}`} alt={`Image of project ${project.titleKey}`}/> : <></>}
+              {project.imgUrl ? <img className="h-44 object-cover" src={`/img/${project.imgUrl}`} alt={`Image of project ${project.titleKey}`}/> : <></>}
             </a>
           </CarouselItem>
         ))}
